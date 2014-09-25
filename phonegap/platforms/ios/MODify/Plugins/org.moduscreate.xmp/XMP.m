95875f43c438aad4dd744b4dd2b1a6a61641cab7
Jay Garcia <jay@moduscreate.com>
2014-09-25 16:19:07 -0400

    plugin XMP added

---
+++ phonegap/platforms/ios/MODify/Plugins/org.moduscreate.xmp/XMP.m
@@ -0,0 +1,22 @@
+//
+//  XMP.m
+//  XMP
+//
+//  Created by Jesus Garcia (jay@moduscreate.com) 9/25/2014.
+//
+//
+
+
+#import "XMP.h"
+
+@implementation XMP {}
+
+- (void) test:(CDVInvokedUrlCommand *) command {
+    NSLog(@"Objective Test");
+    
+    
+}
+
+
+@end
+
---
+++ phonegap/platforms/ios/www/plugins/org.moduscreate.xmp/src/ios/XMP.js
@@ -0,0 +1,16 @@
+cordova.define("org.moduscreate.xmp.XMP", function(require, exports, module) { var argscheck = require('cordova/argscheck'),
+    utils = require('cordova/utils'),
+    exec = require('cordova/exec');
+
+
+var XMP = {
+	test : function() {
+		console.log('JAVA SCRIPT TEST');
+
+		cordova.exec(null, null, 'XMP', 'test', []);
+	}
+};
+
+
+module.exports = XMP;
+});
