e3616e5978ba0b29d673c4fdce408b47f75a6d2c
Jay Garcia <jay@moduscreate.com>
2014-09-25 16:19:00 -0400

    cdv3

--- phonegap/platforms/ios/www/cordova_plugins.js
+++ phonegap/platforms/ios/www/cordova_plugins.js
@@ -1,7 +1,17 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/org.moduscreate.xmp/src/ios/XMP.js",
+        "id": "org.moduscreate.xmp.XMP",
+        "clobbers": [
+            "XMP"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "org.moduscreate.xmp": "0.0.1"
+}
 // BOTTOM OF METADATA
 });
